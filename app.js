5262fc3572adba7e265bf23cd4286d0b13c9cabb
Dikla3107 <dikla.iphone4@gmail.com>
2022-06-07 15:04:35 +0300

    Change

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+function addTax(price) {
+    if (!price || price <= 0) {
+        return "Nothing is for free! Please supply a price";
+    }
+        const vat = 1.17; //maam
+        const total = (price * vat).toFixed(2);
+        return (total);
+    }
